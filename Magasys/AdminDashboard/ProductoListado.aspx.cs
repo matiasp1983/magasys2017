25ed955d3a4caf7c2772e0fced32a488fcfcfd8c
margaritaperez <margarita_perez@outlook.com>
2018-03-30 21:42:14 -0300

    Ejemplo Editar, Visualizar desde la página de ProductoListado.

--- Magasys/AdminDashboard/ProductoListado.aspx.cs
+++ Magasys/AdminDashboard/ProductoListado.aspx.cs
@@ -6,6 +6,12 @@ namespace PL.AdminDashboard
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            /*Prueba con Producto*/
+            /*Session[BLL.Common.Enums.Session.ProductoDiario.ToString()] = new BLL.DiarioBLL().ObtenerDiario(64);
+            Response.Redirect("ProductoDiarioEditar.aspx", false);
+            Response.Redirect("ProductoDiarioVisualizar.aspx", false);*/
+
+            /*Prueba con Revista*/
             Session[BLL.Common.Enums.Session.ProductoRevista.ToString()] = new BLL.RevistaBLL().ObtenerRevista(20);
             Response.Redirect("ProductoRevistaEditar.aspx", false);
         }
