9c64d0bd624e0de252888cb0c97996c0b051c2e2
margaritaperez <margarita_perez@outlook.com>
2018-10-24 21:15:04 -0300

    Forma de Pago BLL.

---
+++ BLL/FormaPagoBLL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BLL.DAL;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class FormaPagoBLL
+    {
+        #region Métodos Públicos
+
+        public List<FormaPago> ObtenerFormasPago()
+        {
+            List<FormaPago> lstFormaPago = null;
+
+            try
+            {
+                using (var rep = new Repository<FormaPago>())
+                {
+                    lstFormaPago = rep.FindAll();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return lstFormaPago;
+        }
+
+        #endregion
+    }
+}
