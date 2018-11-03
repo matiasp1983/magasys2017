d16113fc0b26ef6617ef18d186c31be486771a7f
margaritaperez <margarita_perez@outlook.com>
2018-11-03 17:06:04 -0300

    Actualización Venta.

---
+++ BLL/Filters/VentaFiltro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BLL.Filters
+{
+    public class VentaFiltro
+    {
+        #region Propiedades
+
+        public int ID_VENTA { get; set; }
+        public DateTime? FECHAVENTADESDE { get; set; }
+        public DateTime? FECHAVENTAHASTA { get; set; }
+        public int COD_FORMA_PAGO { get; set; }
+        public int TIPO_DOCUMENTO { get; set; }
+        public int NRO_DOCUMENTO { get; set; }
+        public string APELLIDO { get; set; }
+        public string NOMBRE { get; set; }
+        public string ALIAS { get; set; }
+
+        #endregion
+    }
+}
