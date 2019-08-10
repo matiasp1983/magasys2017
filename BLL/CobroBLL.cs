8220ff8680f39fde030573954120040a141b3122
margaritaperez <margarita_perez@outlook.com>
2019-08-10 18:54:43 -0300

    Se sube clase CobroBLL.

---
+++ BLL/CobroBLL.cs
@@ -0,0 +1,32 @@
+﻿using BLL.DAL;
+using System;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class CobroBLL
+    {
+        #region Métodos Públicos
+
+        public bool AltaCobro(Cobro oCobro, List<DetalleCobro> lstDetalleCobro)
+        {
+            var bRes = false;
+
+            try
+            {
+                lstDetalleCobro.ForEach(x => oCobro.DetalleCobro.Add(x));
+
+                using (var loRepCobro = new Repository<Cobro>())
+                    bRes = loRepCobro.Create(oCobro) != null;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return bRes;
+        }
+
+        #endregion
+    }
+}
