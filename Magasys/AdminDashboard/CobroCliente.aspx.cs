e52bbade1d5dc0459b78405622d0aed6bb5e4bd2
margaritaperez <margarita_perez@outlook.com>
2019-08-19 18:30:34 -0300

    Se sube actualización de cobro.

--- Magasys/AdminDashboard/CobroCliente.aspx.cs
+++ Magasys/AdminDashboard/CobroCliente.aspx.cs
@@ -5,7 +5,6 @@ using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
