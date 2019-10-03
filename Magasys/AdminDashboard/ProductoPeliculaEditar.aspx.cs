be596e2759312cfe0235ea5df3e5e2e23386e7be
margaritaperez <margarita_perez@outlook.com>
2019-10-02 23:00:54 -0300

    Se actualiza Película Edición.

--- Magasys/AdminDashboard/ProductoPeliculaEditar.aspx.cs
+++ Magasys/AdminDashboard/ProductoPeliculaEditar.aspx.cs
@@ -1,8 +1,6 @@
 ﻿using BLL.Common;
 using NLog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -16,6 +14,36 @@ namespace PL.AdminDashboard
         {
             if (!IsPostBack)
                 CargarProductoPeliculaDesdeSession();
+        }
+
+        protected void BtnSubir_Click(object sender, EventArgs e)
+        {
+            // Obtener tamaño de la imagen seleccionada
+            int loTamanioImagen = fuploadImagen.PostedFile.ContentLength;
+            // Obtener tamaño de la imagen en byte
+            byte[] loImagenOriginal = new byte[loTamanioImagen];
+
+            //// Asociar byte a imagen
+            fuploadImagen.PostedFile.InputStream.Read(loImagenOriginal, 0, loTamanioImagen);
+
+            var oImagen = new BLL.DAL.Imagen
+            {
+                IMAGEN1 = loImagenOriginal,
+                NOMBRE = txtTitulo.Text
+            };
+
+            Session.Add(Enums.Session.Imagen.ToString(), oImagen);
+
+            // Covertir la iamgen a un base 64 para mostrarlo en un dato binario
+            string loImagenDataURL64 = "data:image/jpg;base64," + Convert.ToBase64String(loImagenOriginal);
+            imgPreview.ImageUrl = loImagenDataURL64;
+        }
+
+        protected void BtnLimpiarImagen_Click(object sender, EventArgs e)
+        {
+            imgPreview.ImageUrl = "~/AdminDashboard/img/preview_icons.png";
+            txtTitulo.Text = String.Empty;
+            Session.Remove(Enums.Session.Imagen.ToString());
         }
 
         protected void BtnGuardar_Click(object sender, EventArgs e)
@@ -32,7 +60,11 @@ namespace PL.AdminDashboard
                     loResutado = new BLL.PeliculaBLL().ModificarPelicula(oProducto, oPelicula);
 
                     if (loResutado)
+                    {
+                        Session.Remove(Enums.Session.Imagen.ToString());
+                        Session.Remove(Enums.Session.ProductoPelicula.ToString());
                         Page.ClientScript.RegisterStartupScript(GetType(), "Modal", MessageManager.SuccessModal(Message.MsjeProductoSuccessModificacion, "Modificación Producto Película", "ProductoListado.aspx"));
+                    }
                     else
                         Page.ClientScript.RegisterStartupScript(GetType(), "Modal", MessageManager.WarningModal(Message.MsjeProductoFailure));
                 }
@@ -48,12 +80,11 @@ namespace PL.AdminDashboard
                 Logger loLogger = LogManager.GetCurrentClassLogger();
                 loLogger.Error(ex);
             }
-
-            Session.Remove(Enums.Session.ProductoPelicula.ToString());
         }
 
         protected void BtnCancelar_Click(object sender, EventArgs e)
         {
+            Session.Remove(Enums.Session.Imagen.ToString());
             Session.Remove(Enums.Session.ProductoPelicula.ToString());
             Response.Redirect("ProductoListado.aspx", false);
         }
@@ -80,6 +111,14 @@ namespace PL.AdminDashboard
                     CargarGenero(oProductoPelicula.COD_GENERO);
                     CargarAnio(oProductoPelicula.ANIO);
                     txtPrecioPelicula.Text = oProductoPelicula.PRECIO.ToString();
+
+                    if (oProductoPelicula.IMAGEN != null)
+                    {
+                        // Covertir la iamgen a un base 64 para mostrarlo en un dato binario
+                        string loImagenDataURL64 = "data:image/jpg;base64," + Convert.ToBase64String(oProductoPelicula.IMAGEN.IMAGEN1);
+                        imgPreview.ImageUrl = loImagenDataURL64;
+                        txtTitulo.Text = oProductoPelicula.IMAGEN.NOMBRE;
+                    }
                 }
                 else
                     Response.Redirect("ProductoListado.aspx", false);
@@ -111,6 +150,12 @@ namespace PL.AdminDashboard
                 oProducto.DESCRIPCION = txtDescripcion.Text;
             else
                 oProducto.DESCRIPCION = null;
+
+            // Cargar Imagen
+            if ((BLL.DAL.Imagen)Session[Enums.Session.Imagen.ToString()] != null)  // Nueva Imagen
+                oProducto.Imagen = (BLL.DAL.Imagen)Session[Enums.Session.Imagen.ToString()];
+            else if (imgPreview.ImageUrl != "~/AdminDashboard/img/preview_icons.png" && ((BLL.ProductoColeccion)Session[Enums.Session.ProductoColeccion.ToString()]).IMAGEN != null) // Mantenemos la imagen
+                oProducto.COD_IMAGEN = ((BLL.ProductoColeccion)Session[Enums.Session.ProductoColeccion.ToString()]).IMAGEN.ID_IMAGEN;
 
             return oProducto;
         }
@@ -207,6 +252,5 @@ namespace PL.AdminDashboard
         }
 
         #endregion
-
     }
 }
