28795058695fb10dbc02bc84989db17ded8301ab
margaritaperez <margarita_perez@outlook.com>
2021-07-12 19:10:26 -0300

    Se sube actualización del menú del sitio del Kiosco.

--- Magasys/AdminDashboard/MasterPage.Master.designer.cs
+++ Magasys/AdminDashboard/MasterPage.Master.designer.cs
@@ -49,33 +49,6 @@ namespace PL.AdminDashboard {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl liPrincipal;
         
         /// <summary>
-        /// Control liReportes.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liReportes;
-        
-        /// <summary>
-        /// Control liReporteVentaAnualTipoProducto.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liReporteVentaAnualTipoProducto;
-        
-        /// <summary>
-        /// Control liDevolucionProductos.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liDevolucionProductos;
-        
-        /// <summary>
         /// Control liProveedores.
         /// </summary>
         /// <remarks>
